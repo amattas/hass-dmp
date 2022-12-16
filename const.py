a549b026b2a2c307e88d06549b9123e220e38f43
Anthony Mattas <anthony@mattas.net>
2022-12-16 11:08:34 -0500

    Update const.py
    Fixed typos in constant file.

--- const.py
+++ const.py
@@ -12,9 +12,9 @@ CONF_AREA_NUMBER = "area_number"
 CONF_AREA_NAME = "area_name"
 CONF_AREA_ACCTNUM = "area_accountnumber"
 
-CONF_AREA_DISARM_ZONE: "area_disarm_zone"
-CONF_AREA_HOME_ZONE: "area_home_zone"
-CONF_AREA_AWAY_ZONE: "area_away_zone"
+CONF_AREA_DISARM_ZONE = "area_disarm_zone"
+CONF_AREA_HOME_ZONE = "area_home_zone"
+CONF_AREA_AWAY_ZONE = "area_away_zone"
 
 CONF_LOCK_NAME = "door_name"
 CONF_LOCK_NUMBER = "door_number"
