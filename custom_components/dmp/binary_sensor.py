fe1c31bd8c15dc821b8fedbed19e39c9aa948713
Anthony Mattas <anthony@mattas.net>
2022-12-21 13:54:18 -0500

    Update binary_sensor.py

--- custom_components/dmp/binary_sensor.py
+++ custom_components/dmp/binary_sensor.py
@@ -44,20 +44,102 @@ class DMPZoneOpenClose(BinarySensorEntity):
         self._device_class = config.get(CONF_ZONE_CLASS)
         self._panel = listener.getPanels()[str(self._account_number)]
         self._state = False
-        zoneObj = {"zoneName": self._name, "zoneNumber": str(self._number),
-                   "zoneState": self._state}
-        self._panel.updateZone(str(self._number), zoneObj)
+        zoneOpenCloseObj = {
+            "zoneName": self._name,
+            "zoneNumber": str(self._number),
+            "zoneState": self._state
+            }
+        self._panel.updateZone(str(self._number), zoneOpenCloseObj)
 
     async def async_added_to_hass(self):
-        _LOGGER.debug("Registering DMPZone Callback")
+        _LOGGER.debug("Registering DMPZoneOpenClose Callback")
         self._listener.register_callback(self.process_zone_callback)
 
     async def async_will_remove_from_hass(self):
-        _LOGGER.debug("Removing DMPZone Callback")
+        _LOGGER.debug("Removing DMPZoneOpenClose Callback")
         self._listener.remove_callback(self.process_zone_callback)
 
     async def process_zone_callback(self):
-        _LOGGER.debug("DMPZone Callback Executed")
+        _LOGGER.debug("DMPZoneOpenClose Callback Executed")
+        self._state = self._panel.getZone(self._number)["zoneState"]
+        self.async_write_ha_state()
+
+    @property
+    def name(self):
+        """Return the name of the device."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return False
+
+    @property
+    def is_on(self):
+        """Return the state of the device."""
+        _LOGGER.debug("Called DMPZoneOpenClose.is_on: {}".format(self._state))
+        return self._state
+
+    @property
+    def device_class(self):
+        """Return the class of the device"""
+        _LOGGER.debug("Called DMPZoneOpenClose.device_class: {}"
+                      .format(self._device_class))
+        return self._device_class
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        return {
+            "last_contact": self._panel.getContactTime(),
+        }
+
+    @property
+    def unique_id(self):
+        """Return unique ID"""
+        return "dmp-%s-zone-%s-openclose" % (self._account_number,
+                                             self._number)
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device info."""
+        return DeviceInfo(
+            identifiers={
+                (DOMAIN, "dmp-%s-zone-%s" % (self._account_number,
+                                             self._number))
+            },
+            name=self.name,
+            manufacturer='Digital Monitoring Products',
+            via_device=(DOMAIN, "dmp-%s-panel" % (self._account_number))
+        )
+
+
+class DMPZoneBattery(BinarySensorEntity):
+    def __init__(self, listener, config, accountNum):
+        self._listener = listener
+        self._name = "%s Battery" % config.get(CONF_ZONE_NAME)
+        self._number = config.get(CONF_ZONE_NUMBER)
+        self._account_number = accountNum
+        self._device_class = config.get(CONF_ZONE_CLASS)
+        self._panel = listener.getPanels()[str(self._account_number)]
+        self._state = False
+        zoneBatteryObj = {
+            "zoneName": self._name,
+            "zoneNumber": str(self._number),
+            "zoneState": self._state
+            }
+        self._panel.updateZone(str(self._number), zoneBatteryObj)
+
+    async def async_added_to_hass(self):
+        _LOGGER.debug("Registering DMPZoneBattery Callback")
+        self._listener.register_callback(self.process_zone_callback)
+
+    async def async_will_remove_from_hass(self):
+        _LOGGER.debug("Removing DMPZoneBattery Callback")
+        self._listener.remove_callback(self.process_zone_callback)
+
+    async def process_zone_callback(self):
+        _LOGGER.debug("DMPZoneBattery Callback Executed")
         self._state = self._panel.getZone(self._number)["zoneState"]
         self.async_write_ha_state()
 
@@ -81,7 +163,7 @@ class DMPZoneOpenClose(BinarySensorEntity):
     def device_class(self):
         """Return the class of the device"""
         _LOGGER.debug("Called DMPZone.device_class: {}"
-                      .format(self._device_class))
+                      .format("battery"))
         return self._device_class
 
     @property
@@ -94,8 +176,10 @@ class DMPZoneOpenClose(BinarySensorEntity):
     @property
     def unique_id(self):
         """Return unique ID"""
-        return "dmp-%s-zone-%s-openclose" % (self._account_number,
-                                             self._number)
+        return "dmp-%s-zone-%s-battery" % (
+            self._account_number,
+            self._number
+            )
 
     @property
     def device_info(self) -> DeviceInfo:
