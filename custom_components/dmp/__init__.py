272f05a21fd54873bdbfe4297f38190d41d1b8a8
Anthony Mattas <anthony@mattas.net>
2022-12-22 09:43:31 -0500

    Update __init__.py

--- custom_components/dmp/__init__.py
+++ custom_components/dmp/__init__.py
@@ -64,37 +64,13 @@ async def options_update_listener(hass, entry):
     if entry.options:
         """Handle options update."""
         config = dict(entry.data)
-        _LOGGER.debug("Option Update Listener Config %s" % config)
         options = dict(entry.options)
-        _LOGGER.debug("Option Update Listener Options %s" % entry.options)
         config["zones"] = options["zones"]
-        _LOGGER.debug("Option Update Listener Config %s" % config)
-        output = hass.config_entries.async_update_entry(
-            entry, 
-            data=config, 
+        await hass.config_entries.async_update_entry(
+            entry,
+            data=config,
             options={}
             )
-        _LOGGER.debug("New Config %s" % output)
-        _LOGGER.debug("New Config %s" % config)
-#        for option in entry.options:
-#            _LOGGER.debug("Option added %s" % option)
-            # hass.data[DOMAIN][CONF_ZONES].insert(entry_id)
-        # await entry.async_reload(entry.entry_id)
-
-
-# async def async_unload_entry(hass, entry):
-#     """Unload a config entry."""
-#     unload_ok = all(
-#         await asyncio.gather(
-#             *[hass.config_entries.async_forward_entry_unload(
-#                 entry, "binary_sensor"
-#                 )]
-#         )
-#     )
-#     hass.data[DOMAIN][entry.entry_id]["unsub_options_update_listener"]()
-#     if unload_ok:
-#         hass.data[DOMAIN][CONF_ZONES].pop(entry.entry_id)
-#     return unload_ok
 
 
 class DMPPanel():
