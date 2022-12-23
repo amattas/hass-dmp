83340326b7725521b95243462cfdcfdd87205f50
Anthony Mattas <anthony@mattas.net>
2022-12-22 22:38:16 -0500

    Update binary_sensor.py

--- custom_components/dmp/binary_sensor.py
+++ custom_components/dmp/binary_sensor.py
@@ -284,7 +284,7 @@ class DMPZoneTrouble(BinarySensorEntity):
     async def async_will_remove_from_hass(self):
         _LOGGER.debug("Removing DMPZoneTrouble Callback")
         device_registry_items = dr.DeviceRegistryItems()
-        device_identifiers = list(self.device_info.identifiers)
+        device_identifiers = list(self.device_info["identifiers"])
         for i in device_identifiers:
             device_registry_items.get_entry(i)
             _LOGGER.debug("Found item %s" % i)
