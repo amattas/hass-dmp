3cf71b7a1a09cf45a3cf1cfe6a121f055962c195
NebulaGazer <133916031+NebulaGazer@users.noreply.github.com>
2025-01-09 18:25:18 -0500

    fix low battery zone status showing as bypassed. readme updates (#36)
    * Fixes low battery being reported as bypassed
    * Additional documentation updates

--- custom_components/dmp/__init__.py
+++ custom_components/dmp/__init__.py
@@ -271,7 +271,7 @@ class DMPPanel():
             zone_state = 'Bypass'
         elif (self.getBatteryZone(zoneNum)
               and self.getBatteryZone(zoneNum)['zoneState']):
-            zone_state = 'Bypass'
+            zone_state = 'Low Battery'
         elif (self.getOpenCloseZone(zoneNum)
               and self.getOpenCloseZone(zoneNum)['zoneState']):
             zone_state = 'Open'
